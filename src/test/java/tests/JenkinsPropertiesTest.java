46c5c254d82a231cb2cf09f5dc596d530e6c1227
Valigura <Volodymyr.valigra@gmail.com>
2023-11-29 12:57:15 +0200

    Add console output

--- src/test/java/tests/JenkinsPropertiesTest.java
+++ src/test/java/tests/JenkinsPropertiesTest.java
@@ -23,10 +23,10 @@ public class JenkinsPropertiesTest {
     void beforeEach(){
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Configuration.browserSize = System.setProperty("browser_extension","1920x1080");
-        Configuration.browser = System.setProperty("browser_name","chrome");
-        Configuration.browserVersion = System.setProperty("browser_version","100.0");
-        Configuration.remote = System.setProperty("selenoid_url","https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.browserSize = System.getProperty("browser_extension","1920x1080");
+        Configuration.browser = System.getProperty("browser_name","chrome");
+        Configuration.browserVersion = System.getProperty("browser_version","100.0");
+        Configuration.remote = System.getProperty("selenoid_url","https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -55,12 +55,7 @@ public class JenkinsPropertiesTest {
         String city = "Karnal";
         String subjects = "English";
 
-        System.out.println("----------------------PARAMETERS START---------------------");
-        System.out.println("browser_extension : " + System.getProperty("browser_extension"));
-        System.out.println("browser_name : "+System.getProperty("browser_name"));
-        System.out.println("browser_version : "+System.getProperty("browser_version"));
-        System.out.println("selenoid_url : "+System.getProperty("selenoid_url"));
-        System.out.println("----------------------PARAMETERS END---------------------");
+
 
         new RegistrationFormPage().openPage()
                 .setFirstName(firstName)
@@ -82,6 +77,5 @@ public class JenkinsPropertiesTest {
                 .verifyResult("Gender", "Male")
                 .verifyResult("Mobile",mobile )
                 .verifyResult("Date of Birth", "05 June,1991");
-
     }
 }
